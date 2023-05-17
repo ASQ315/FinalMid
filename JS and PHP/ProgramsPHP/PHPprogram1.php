4a605352bf11e37e10b2a9ada6d910c9281b57fd
ASQ315 <133872669+ASQ315@users.noreply.github.com>
2023-05-17 19:18:31 -0400

    Add files via upload

--- JS and PHP/ProgramsPHP/PHPprogram1.php
+++ JS and PHP/ProgramsPHP/PHPprogram1.php
@@ -1,4 +1,3 @@
-<html>
 
 <head>
     <title>Angel Santiago Web </title>
@@ -8,26 +7,46 @@
     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Ubuntu:wght@300&display=swap"
         rel="stylesheet">
 
-    <link rel="stylesheet" type="text/css" href="stylesheetExample1.css">
+    <link rel="stylesheet" type="text/css" href="../stylesheetExample1.css">
 </head>
 
 <body>
     <form method="post">
-        Enter First Number:
+        Number 1:
         <input type="number" name="number1" /><br><br>
-        Enter Second Number:
+        Number 2:
         <input type="number" name="number2" /><br><br>
-        <input type="submit" name="submit" value="Add">
+        
+        <button name="submit" >
+    <span>Example PHP</span>
+    <div class="top"></div>
+    <div class="left"></div>
+    <div class="bottom"></div>
+    <div class="right"></div>
+</button>
+
     </form>
     <?php  
     if(isset($_POST['submit']))  
     {  
         $number1 = $_POST['number1'];  
         $number2 = $_POST['number2'];  
-        $sum =  $number1+$number2;     
-echo "The sum of " .$number1 ." and " .$number2 ."is: ".$sum;   
+
+        $sum =  $number1+$number2;
+        $rest = $number1-$number2;
+        $mult = $number1*$number2;
+        $div = $number1/$number2;
+
+echo "The adition of " .$number1 ." and " .$number2 ." is: ".$sum;
+echo "<br> The subtraction of " .$number1 ." and " .$number2 ." is: ".$rest;
+echo "<br> The multiplicacion of " .$number1 ." and " .$number2 ." is: ".$mult;
+echo "<br>The division of " .$number1 ." and " .$number2 ." is: ".$div; 
+
 }  
 ?>
+</body>
+
+</html>
 </body>
 
 </html>
--- JS and PHP/ProgramsPHP/PHPprogram3.php
+++ JS and PHP/ProgramsPHP/PHPprogram3.php
@@ -1,34 +1,74 @@
-<?php
-// Function to convert Celsius to Fahrenheit
-function celsiusToFahrenheit($celsius) {
-  return ($celsius * 9/5) + 32;
-}
 
-// Function to convert Fahrenheit to Celsius
-function fahrenheitToCelsius($fahrenheit) {
-  return ($fahrenheit - 32) * 5/9;
-}
+<html>
 
-// Handle the temperature conversion
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-  // Get the temperature and unit from the form
-  $temperaturePHP = floatval($_POST["temperaturePHP"]);
-  $unitPHP = $_POST["unitPHP"];
+<head>
+    <title>Angel Santiago Web </title>
 
-  $resultPHP = "";
+    <link rel="../images/shortcut icon" type="image/x-icon" href="../images/pokeball.png">
 
-  // Check the unit and perform the conversion
-  if ($unitPHP === "C") {
-    // Convert Celsius to Fahrenheit
-    $resultPHP = celsiusToFahrenheit($temperaturePHP) . "°F";
-  } elseif ($unitPHP === "F") {
-    // Convert Fahrenheit to Celsius
-    $resultPHP = fahrenheitToCelsius($temperaturePHP) . "°C";
-  } else {
-    $resultPHP = "Invalid unit entered. Please enter 'C' for Celsius or 'F' for Fahrenheit.";
-  }
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Ubuntu:wght@300&display=swap"
+        rel="stylesheet">
 
-  // Output the result
-  echo $resultPHP;
-}
+    <link rel="stylesheet" type="text/css" href="../stylesheetExample1.css">
+</head>
+
+<body>
+    <form method="post">
+        Enter the Tempeture:
+        <input type="number" name="temperaturePHP" /><br><br>
+
+        Enter the unit (C o F):
+        <input type="number" name="unitPHP" /><br><br>
+
+        <button name="submit" >
+    <span>Example PHP</span>
+    <div class="top"></div>
+    <div class="left"></div>
+    <div class="bottom"></div>
+    <div class="right"></div>
+</button>
+        
+    </form>
+            <?php
+            // Function to convert Celsius to Fahrenheit
+            function celsiusToFahrenheit($celsius) {
+              $tempFar = ($celsius * 9/5) + 32;
+
+              echo "" .$celsius. " is equal to " .$tempFar. " F";
+            }
+
+            // Function to convert Fahrenheit to Celsius
+            function fahrenheitToCelsius($fahrenheit) {
+              $tempCel = ($fahrenheit - 32) * 5/9;
+
+
+              echo "" .$fahrenheit. " is equal to " .$tempCel. " C";
+            }
+
+            // Handle the temperature conversion
+            if(isset($_POST['submit'])) {
+              // Get the temperature and unit from the form
+              $temperaturePHP = floatval($_POST["temperaturePHP"]);
+              $unitPHP = $_POST["unitPHP"];
+
+              $resultPHP = "";
+
+              // Check the unit and perform the conversion
+              if ($unitPHP === "C") {
+                // Convert Celsius to Fahrenheit
+                $resultPHP = celsiusToFahrenheit($temperaturePHP);
+              } elseif ($unitPHP === "F") {
+                // Convert Fahrenheit to Celsius
+                $resultPHP = fahrenheitToCelsius($temperaturePHP);
+              } else {
+                $resultPHP = "Invalid unit entered. Please enter 'C' for Celsius or 'F' for Fahrenheit.";
+              }
+
+              // Output the result
+              echo $resultPHP;
+            }
 ?>
+?>
+</body>
+
+</html>
